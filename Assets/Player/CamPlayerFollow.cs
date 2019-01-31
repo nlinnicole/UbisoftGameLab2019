8e7779c0a20ec0e0b7293d1f90a5f65dbbe25edd
scott <scottjamessmith05@gmail.com>
2019-01-31 16:43:28 -0500

    added smooth follow cam

---
+++ Assets/Player/CamPlayerFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CamPlayerFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float lerpAmount;
+    Vector3 target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        target = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, target, lerpAmount);
+    }
+}
