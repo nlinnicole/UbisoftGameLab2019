5319dd71dada924c0a44bc7181c37cd3ca6dacbc
scott <scottjamessmith05@gmail.com>
2019-01-30 20:11:45 -0500

    player controller basics

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 1f;
+    public float moveDrag = 0.1f;
+    public float accelerationFactor = 1;
+    public float jumpForce = 5;
+    public bool isGrounded = false;
+    public float groundDetectDistance = 0.5f;
+    public float gravityMagnitude;
+    Vector3 velocity;
+    Vector3 acceleration;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //movement
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            velocity.x += 1 * moveSpeed;
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            velocity.x -= 1 * moveSpeed;
+        }
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            velocity.z += 1 * moveSpeed;
+        }
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            velocity.z -= 1 * moveSpeed;
+        }
+
+        velocity = Vector3.ClampMagnitude(velocity, 1 * moveSpeed); //clamping instead of normalizing
+        velocity /= moveDrag; //reduce velocity vector to look like drag
+        transform.position += velocity; //apply velocity to transform
+
+
+        if(Physics.Raycast(transform.position, Vector3.down, groundDetectDistance))
+        {
+            isGrounded = true;
+        }
+        if(Input.GetAxisRaw("Jump") > 0 && isGrounded)
+        {
+            isGrounded = false;
+            gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
+        }
+        
+
+    }
+}
