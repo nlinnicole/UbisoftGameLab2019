94e3a74a8c8d3f713b6b8ccf33777891e5b11453
scott <scottjamessmith05@gmail.com>
2019-01-31 19:33:29 -0500

    fixed bug, player rotation inproved

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -14,8 +14,6 @@ public class PlayerController : MonoBehaviour
     public float groundDetectDistance = 0.5f;
     public float rotationLerpAmount;
     Vector3 velocity;
-    Vector3 faceVelocity;
-    Vector3 acceleration;
     Vector3 faceDirection;
 
     void Start()
@@ -23,70 +21,69 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    void Update()
-    {
-        
-    }
-
     void FixedUpdate()
     {
+        faceDirection = Vector3.zero;
         if (isGrounded)
         {
             //movement
             if (Input.GetAxisRaw("Horizontal") > 0)
             {
                 velocity.x += 1 * moveSpeed;
-                faceVelocity.x += 1 * rotationSpeed;
+                faceDirection.x = 1;
             }
             if (Input.GetAxisRaw("Horizontal") < 0)
             {
                 velocity.x -= 1 * moveSpeed;
-                faceVelocity.x -= 1 * rotationSpeed;
+                faceDirection.x = -1;
             }
             if (Input.GetAxisRaw("Vertical") > 0)
             {
                 velocity.z += 1 * moveSpeed;
-                faceVelocity.z += 1 * rotationSpeed;
+                faceDirection.z = 1;
             }
             if (Input.GetAxisRaw("Vertical") < 0)
             {
                 velocity.z -= 1 * moveSpeed;
-                faceVelocity.z -= 1 * rotationSpeed;
+                faceDirection.z = -1;
             }
 
             velocity /= moveDrag; //reduce velocity vector to look like drag
-            faceVelocity /= moveDrag; //reduce velocity vector to look like drag
         }
         else {
             //reduced movement when jumping
             if (Input.GetAxisRaw("Horizontal") > 0)
             {
                 velocity.x += (1 * moveSpeed) / jumpMovementReduction;
-                faceVelocity.x += (1 * rotationSpeed);
+                faceDirection.x = 1;
             }
             if (Input.GetAxisRaw("Horizontal") < 0)
             {
                 velocity.x -= (1 * moveSpeed) / jumpMovementReduction;
-                faceVelocity.x -= (1 * rotationSpeed);
+                faceDirection.x = -1;
             }
             if (Input.GetAxisRaw("Vertical") > 0)
             {
                 velocity.z += (1 * moveSpeed) / jumpMovementReduction;
-                faceVelocity.z += (1 * rotationSpeed);
+                faceDirection.z = 1;
             }
             if (Input.GetAxisRaw("Vertical") < 0)
             {
                 velocity.z -= (1 * moveSpeed) / jumpMovementReduction;
-                faceVelocity.z -= (1 * moveSpeed);
+                faceDirection.z = -1;
             }
         }
 
+        faceDirection.Normalize();
+        if(faceDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(faceDirection), rotationLerpAmount);
+        }
 
         velocity = Vector3.ClampMagnitude(velocity, 1 * moveSpeed); //clamping instead of normalizing
-        faceVelocity = Vector3.ClampMagnitude(faceVelocity, 1 * moveSpeed); //clamping instead of normalizing
         transform.position += velocity; //apply velocity to transform
 
-
+        //check if on the ground
         if(Physics.Raycast(transform.position, Vector3.down, groundDetectDistance))
         {
             isGrounded = true;
@@ -96,10 +93,5 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
             gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpForce);
         }
-
-
-        faceDirection = Vector3.Lerp(transform.position + transform.forward, transform.position + faceVelocity, rotationLerpAmount);
-        transform.LookAt(faceDirection);
-
     }
 }
