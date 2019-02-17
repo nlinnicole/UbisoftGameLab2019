ea37a2e88e77e207dd822effa322d8efe30550bf
scott <scottjamessmith05@gmail.com>
2019-02-17 18:33:39 -0500

    added rope gas when broken & it flies around

--- Assets/Player/RopeGenerator.cs
+++ Assets/Player/RopeGenerator.cs
@@ -12,14 +12,56 @@ public class RopeGenerator : MonoBehaviour
     public int ropeResolution;
     float ropeLength;
 
+    public bool isBroken = false;
+    public GameObject ropeGas;
 
+    public GameObject[] ropeJoints;
+    public Transform[] ropeJointsTrans;
+    public int brokenJoint;
+    public bool startedGas = false;
+
+    public float minGasForce;
+    public float maxGasForce;
+
+    void FixedUpdate()
+    {
+        if(isBroken && !startedGas)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                if (transform.GetChild(i).GetComponent<RopeJoint>().broken)
+                {
+                    brokenJoint = i;
+                    break;
+                }
+            }
+
+            GameObject gas = Instantiate(ropeGas, ropeJointsTrans[brokenJoint].transform);
+            GameObject gas2 = Instantiate(ropeGas, ropeJointsTrans[brokenJoint+2].transform);
+            startedGas = true;
+
+            transform.GetChild(brokenJoint - 1).gameObject.AddComponent<ConstantForce>();
+            transform.GetChild(brokenJoint+1).gameObject.AddComponent<ConstantForce>();
+        }
+
+        if(startedGas)
+        {
+            transform.GetChild(brokenJoint - 1).gameObject.GetComponent<ConstantForce>().relativeForce = new Vector3(Random.Range(minGasForce, maxGasForce), Random.Range(minGasForce, maxGasForce), Random.Range(minGasForce, maxGasForce));
+            transform.GetChild(brokenJoint + 1).gameObject.GetComponent<ConstantForce>().relativeForce = new Vector3(Random.Range(minGasForce, maxGasForce), Random.Range(minGasForce, maxGasForce), Random.Range(minGasForce, maxGasForce));
+        }
+    }
+
+    void Start()
+    {
+        ropeJointsTrans = transform.GetComponentsInChildren<Transform>();
+    }
 
 
     public void generate()
     {
         ropeLength = Vector3.Distance(ropeEnd.transform.position, ropeStart.transform.position);
 
-        GameObject[] ropeJoints = new GameObject[ropeResolution];
+        ropeJoints = new GameObject[ropeResolution];
 
         gameObject.transform.position = ropeStart.transform.position;
 
---
+++ Assets/Player/RopeJoint.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeJoint : MonoBehaviour
+{
+
+    public bool broken = false;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!transform.parent.GetComponent<RopeGenerator>().isBroken && collision.gameObject.layer == 14)
+        {
+            transform.parent.GetComponent<RopeGenerator>().isBroken = true;
+            broken = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
