74d38047baa32a0f527fed6a307aa5a298344910
scott <scottjamessmith05@gmail.com>
2019-02-20 15:09:29 -0500

    roomgen and choices working

--- Assets/Room & Game Manager/GameManager.cs
+++ Assets/Room & Game Manager/GameManager.cs
@@ -4,29 +4,38 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
     public int baseRoomSize = 40;
     public int amountOfRooms = 10;
 
+    [Header("Team 1")]
     public GameObject team1;
     public GameObject team1StartRoom;
     public GameObject Team1Space;
-    public int Team1RoomNumber = 0;
-    
+
+    [Header("Team 2")]
     public GameObject team2;
     public GameObject team2StartRoom;
     public GameObject Team2Space;
-    public int Team2RoomNumber = 0;
 
+    [Header("Rooms")]
     public GameObject[] roomArray;
+
     public GameObject[] Team1Rooms;
+    int[,] team1RoomRefs;
+    public int Team1RoomNumber = 0;
+
     public GameObject[] Team2Rooms;
+    int[,] team2RoomRefs;
+    public int Team2RoomNumber = 0;
 
     GameObject team1Rooms;
     GameObject team2Rooms;
 
     public GameObject team1Start;
     public GameObject team2Start;
+
+    bool team1InFirstRoom = true;
+    bool team2InFirstRoom = true;
 
     void Start()
     {
@@ -50,105 +59,113 @@ public class GameManager : MonoBehaviour
         team1Rooms.name = "Team1 Rooms";
         team2Rooms.name = "Team2 Rooms";
 
-        team1Start = Instantiate(team1StartRoom);
-        team2Start = Instantiate(team2StartRoom, new Vector3(-baseRoomSize, 0, 0), Quaternion.identity);
+        //team1Start = Instantiate(team1StartRoom);
+        //team2Start = Instantiate(team2StartRoom, new Vector3(-baseRoomSize, 0, 0), Quaternion.identity);
 
-        Team1Rooms = new GameObject[(amount * 2)-1];
-        Team2Rooms = new GameObject[(amount * 2)-1];
+        Team1Rooms = new GameObject[(amount * 2)+1];
+        Team2Rooms = new GameObject[(amount * 2)+1];
 
-        for (int i = 1, k = 1; i < amount * 2; i+=2, k++)
+        team1RoomRefs = new int[amount, 2];
+        team2RoomRefs = new int[amount, 2];
+
+        Team1RoomNumber = 0;
+        Team2RoomNumber = 0;
+
+        for (int i = 0, k = 1; i < amount * 2; i += 2, k++)
         {
-            if(i < (amount * 2) -1)
-            {
-                //randomly choose a room
-                GameObject roomToBuild = roomArray[Random.Range(0, roomArray.Length)];
-                //create room
-                Team1Rooms[i] = Instantiate(roomToBuild, new Vector3(0, 0, baseRoomSize * k), Quaternion.identity, team1Rooms.transform) as GameObject;
-                //change name
-                Team1Rooms[i].gameObject.name += " (Option 1)";
-                Team1Rooms[i].SetActive(false);
 
+            //randomly choose a room
+            GameObject roomToBuild = roomArray[Random.Range(0, roomArray.Length)];
+            //create room
+            Team1Rooms[i] = Instantiate(roomToBuild, new Vector3(0, 0, baseRoomSize * k), Quaternion.identity, team1Rooms.transform) as GameObject;
+            //change name
+            Team1Rooms[i].gameObject.name += " (Option 1)";
+            Team1Rooms[i].SetActive(false);
 
-                roomToBuild = roomArray[Random.Range(0, roomArray.Length)];
-                Team1Rooms[i+1] = Instantiate(roomToBuild, new Vector3(0, 0, baseRoomSize * k), Quaternion.identity, team1Rooms.transform) as GameObject;
-                Team1Rooms[i+1].gameObject.name += " (Option 2)";
-                Team1Rooms[i+1].SetActive(false);
+            roomToBuild = roomArray[Random.Range(0, roomArray.Length)];
+            Team1Rooms[i+1] = Instantiate(roomToBuild, new Vector3(0, 0, baseRoomSize * k), Quaternion.identity, team1Rooms.transform) as GameObject;
+            Team1Rooms[i+1].gameObject.name += " (Option 2)";
+            Team1Rooms[i+1].SetActive(false);
 
+            //team2
+            roomToBuild = roomArray[Random.Range(0, roomArray.Length)];
+            Team2Rooms[i] = Instantiate(roomToBuild, new Vector3(-baseRoomSize, 0, baseRoomSize * k), Quaternion.identity, team2Rooms.transform) as GameObject;
+            Team2Rooms[i].gameObject.name += " (Option 1)";
+            Team2Rooms[i].SetActive(false);
 
-                //team2
-                roomToBuild = roomArray[Random.Range(0, roomArray.Length)];
-                Team2Rooms[i] = Instantiate(roomToBuild, new Vector3(-baseRoomSize, 0, baseRoomSize * k), Quaternion.identity, team2Rooms.transform) as GameObject;
-                Team2Rooms[i].gameObject.name += " (Option 1)";
-                Team2Rooms[i].SetActive(false);
-
-                roomToBuild = roomArray[Random.Range(0, roomArray.Length)];
-                Team2Rooms[i+1] = Instantiate(roomToBuild, new Vector3(-baseRoomSize, 0, baseRoomSize * k), Quaternion.identity, team2Rooms.transform) as GameObject;
-                Team2Rooms[i+1].gameObject.name += " (Option 2)";
-                Team2Rooms[i+1].SetActive(false);
-
-            }
+            roomToBuild = roomArray[Random.Range(0, roomArray.Length)];
+            Team2Rooms[i+1] = Instantiate(roomToBuild, new Vector3(-baseRoomSize, 0, baseRoomSize * k), Quaternion.identity, team2Rooms.transform) as GameObject;
+            Team2Rooms[i+1].gameObject.name += " (Option 2)";
+            Team2Rooms[i+1].SetActive(false);
+           
         }
     }
 
+    short adderTeam1 = 0;
+    short adderTeam2 = 0;
     void FixedUpdate()
     {
-        if (Team1RoomNumber == 0)
+        if(team1InFirstRoom)
         {
-            if (team1Start.GetComponent<Room>().door1Trigger.GetComponent<DoorTrigger>().doorChosen)
+            if (team1StartRoom.GetComponent<Room>().door1Trigger.GetComponent<DoorTrigger>().doorChosen)
             {
-                Team1Rooms[Team1RoomNumber + 1].SetActive(true);
-                Team1RoomNumber++;
+                Team1Rooms[0].SetActive(true);
+                team1InFirstRoom = false;
             }
-            else if (team1Start.GetComponent<Room>().door2Trigger.GetComponent<DoorTrigger>().doorChosen)
+            else if (team1StartRoom.GetComponent<Room>().door2Trigger.GetComponent<DoorTrigger>().doorChosen)
             {
-                Team1Rooms[Team1RoomNumber + 2].SetActive(true);
-                Team1RoomNumber++;
+                Team1Rooms[1].SetActive(true);
+                team1InFirstRoom = false;
+                adderTeam1 = 1;
             }
         }
-
-        if (Team2RoomNumber == 0)
-        {
-            if (team2Start.GetComponent<Room>().door1Trigger.GetComponent<DoorTrigger>().doorChosen)
-            {
-                Team2Rooms[Team2RoomNumber + 1].SetActive(true);
-                Team2RoomNumber++;
-            }
-            else if (team2Start.GetComponent<Room>().door2Trigger.GetComponent<DoorTrigger>().doorChosen)
-            {
-                Team2Rooms[Team2RoomNumber + 2].SetActive(true);
-                Team2RoomNumber++;
-            }
-        }
-
-
-        if (Team1RoomNumber > 0)
+        else
         {
             //check which room the players choose
-            //team 1
-            if (Team1Rooms[Team1RoomNumber].GetComponent<Room>().door1Trigger.GetComponent<DoorTrigger>().doorChosen)
+            if (Team1Rooms[Team1RoomNumber + adderTeam1].GetComponent<Room>().door1Trigger.GetComponent<DoorTrigger>().doorChosen)
             {
-                Team1Rooms[Team1RoomNumber + 1].SetActive(true);
                 Team1RoomNumber += 2;
+                adderTeam1 = 0;
+                Team1Rooms[Team1RoomNumber + adderTeam1].SetActive(true);
             }
-            else if (Team1Rooms[Team1RoomNumber].GetComponent<Room>().door2Trigger.GetComponent<DoorTrigger>().doorChosen)
+            else if (Team1Rooms[Team1RoomNumber + adderTeam1].GetComponent<Room>().door2Trigger.GetComponent<DoorTrigger>().doorChosen)
             {
-                Team1Rooms[Team1RoomNumber + 2].SetActive(true);
                 Team1RoomNumber += 2;
+                adderTeam1 = 1;
+                Team1Rooms[Team1RoomNumber + adderTeam1].SetActive(true);
             }
+
         }
 
-        if (Team2RoomNumber > 0)
+        //team2
+        if (team2InFirstRoom)
         {
-            //team 2
-            if (Team2Rooms[Team2RoomNumber].GetComponent<Room>().door1Trigger.GetComponent<DoorTrigger>().doorChosen)
+            if (team1StartRoom.GetComponent<Room>().door1Trigger.GetComponent<DoorTrigger>().doorChosen)
             {
-                Team2Rooms[Team2RoomNumber + 1].SetActive(true);
-                Team2RoomNumber += 2;
+                Team1Rooms[0].SetActive(true);
+                team2InFirstRoom = false;
             }
-            else if (Team2Rooms[Team2RoomNumber].GetComponent<Room>().door2Trigger.GetComponent<DoorTrigger>().doorChosen)
+            else if (team1StartRoom.GetComponent<Room>().door2Trigger.GetComponent<DoorTrigger>().doorChosen)
             {
-                Team2Rooms[Team2RoomNumber + 2].SetActive(true);
-                Team2RoomNumber += 2;
+                Team1Rooms[1].SetActive(true);
+                team2InFirstRoom = false;
+                adderTeam2 = 1;
+            }
+        }
+        else
+        {
+            //check which room the players choose
+            if (Team1Rooms[Team1RoomNumber + adderTeam2].GetComponent<Room>().door1Trigger.GetComponent<DoorTrigger>().doorChosen)
+            {
+                Team1RoomNumber += 2;
+                adderTeam2 = 0;
+                Team1Rooms[Team1RoomNumber + adderTeam2].SetActive(true);
+            }
+            else if (Team1Rooms[Team1RoomNumber + adderTeam2].GetComponent<Room>().door2Trigger.GetComponent<DoorTrigger>().doorChosen)
+            {
+                Team1RoomNumber += 2;
+                adderTeam2 = 1;
+                Team1Rooms[Team1RoomNumber + adderTeam2].SetActive(true);
             }
         }
     }
