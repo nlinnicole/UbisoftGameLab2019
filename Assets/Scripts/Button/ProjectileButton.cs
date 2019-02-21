e8e02476500a4d6ebe497fcc25d50482a1f1685c
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2019-02-20 23:58:44 -0500

    - projectile button

---
+++ Assets/Scripts/Button/ProjectileButton.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileButton : MonoBehaviour
+{
+
+    [SerializeField]
+    private int amountShot;
+    [SerializeField]
+    private int durationBetweenShot;
+    [SerializeField]
+    private GameObject weapon;
+    [SerializeField]
+    private GameObject location;
+
+    private float timer;
+    private bool shooting = false;
+    private int counter; 
+
+    void Start()
+    {
+        timer = Time.time;
+    }
+
+    void Update()
+    {
+        //Will shoot when button is pressed
+        if (shooting && Time.time > timer + durationBetweenShot)
+        {
+            if(counter != amountShot)
+            {
+                GameObject weaponObject = Instantiate(weapon, location.transform.position, Quaternion.identity);
+                counter++;
+                timer = Time.time;
+            }
+            else
+            {
+                //no more shots require
+                shooting = false;
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        timer = Time.time;
+        counter = 0;
+        shooting = true;
+    }
+}
---
+++ Assets/Scripts/Element/ProjectileWeapon.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileWeapon : MonoBehaviour
+{
+    [SerializeField]
+    private int shotSpeed;
+    [SerializeField]
+    private Vector3 direction;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * shotSpeed, Space.World);
+    }
+}
