303cba30e0596674bb6d9315338958536d4b88d8
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2019-03-05 16:31:06 -0500

    fixed height issue with moving blocks

--- Assets/Scripts/Element/MovingBlock.cs
+++ Assets/Scripts/Element/MovingBlock.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ * A block that the player can pick up and drag along
+ */
 public class MovingBlock : MonoBehaviour
 {
+    //the player which picked up the block
     private GameObject player;
     private Vector3 distance;
+
+    //the height of the block 
+    private float height; 
+
     void Start()
     {
-        
+        height = (GetComponent<Collider>().bounds.size.y)/2;
     }
 
     void Update()
     {
         if(player != null)
         {
-            transform.position = player.transform.position + distance;
+            transform.position = new Vector3(player.transform.position.x + distance.x, height, player.transform.position.z + distance.z);
         }
     }
 
