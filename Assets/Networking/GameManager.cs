464b222d87c7c43b43fc800ff9f114527de26360
Ricardo Liganor <ricardo.liganor@hotmail.com>
2019-03-17 02:29:30 -0400

    Network player instantiation begining, important changes to player controller scripts to add networking

--- Assets/Networking/GameManager.cs
+++ Assets/Networking/GameManager.cs
@@ -102,9 +102,16 @@ namespace Concordia1.Gamelab
             }
             else
             {
-                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
-                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
-                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+                if (PlayerController.LocalPlayerInstance == null)
+                {
+                    Debug.LogFormat("We are Instantiating LocalPlayer from {0}", SceneManagerHelper.ActiveSceneName);
+                    // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                    PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(50f, 5f, 10f), Quaternion.identity, 0);
+                }
+                else
+                {
+                    Debug.LogFormat("Ignoring scene load for {0}", SceneManagerHelper.ActiveSceneName);
+                }
             }
         }
 
