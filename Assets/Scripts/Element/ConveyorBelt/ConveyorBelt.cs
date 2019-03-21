64bb04362c656d8fe5c71431aa0e7be49a6025c2
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2019-03-20 20:00:52 -0400

    - conveyor belt

---
+++ Assets/Scripts/Element/ConveyorBelt/ConveyorBelt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    [SerializeField]
+    private List<ConveyorBlock> blocks;
+
+    [SerializeField]
+    private GameObject edge;
+
+    private float length;
+    private Vector3 lengthVec; 
+
+    void Start()
+    {
+        length = GetComponent<Collider>().bounds.size.x;
+        lengthVec = new Vector3(length, 0, 0);
+    }
+
+    void Update()
+    {
+        for(int i = 0; i< blocks.Count; i++)
+        {
+            if(blocks[i].transform.position.x > edge.transform.position.x)
+            {
+                blocks[i].transform.Translate(blocks[i].transform.position - lengthVec);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Element/ConveyorBelt/ConveyorBlock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBlock : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed, Space.World);
+    }
+
+}
