77af48855c0e799648dce55cc49e267efe5a4ff6
DESKTOP-75EJLIQ\Hiu Tung Lam Emily <e.mily.lam@hotmail.com>
2019-03-21 19:58:38 -0400

    coveyer belt scene

---
+++ Assets/Scripts/Element/Axe.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    [SerializeField]
+    private float axeLength = 5f;
+    [SerializeField]
+    private GameObject axeObject;
+    [SerializeField]
+    private float rotateSpeed = 0.5f;
+
+    private GameObject axe;
+
+    void Start()
+    {
+        axe = Instantiate(axeObject, new Vector3(transform.position.x, transform.position.y + (axeLength / 2), transform.position.z), Quaternion.identity);
+    }
+
+    void Update()
+    {
+        axe.transform.RotateAround(transform.position, Vector3.forward, 180*Time.deltaTime * rotateSpeed);
+    }
+}
