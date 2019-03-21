db4c1899f700e340c5aeba46f530ddc42bf5bf9b
scott <scottjamessmith05@gmail.com>
2019-03-21 15:44:41 -0400

    pull level

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+
+    public float speed;
+    public float amount;
+    public float offset;
+
+    private void FixedUpdate()
+    {
+        this.transform.position += new Vector3(Mathf.Cos(Time.time * speed) + offset, 0, 0) * amount;
+    }
+}
---
+++ Assets/Scripts/PullObjZone.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullObjZone : MonoBehaviour
+{
+    public GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "LevelTrigger")
+        {
+            door.transform.position += new Vector3(0, 6, 0);
+            Destroy(other.GetComponent<BoxCollider>());
+        }
+    }
+}
