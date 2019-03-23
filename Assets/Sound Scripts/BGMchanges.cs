9130d23219aead6b7bd8adf24a4669c060963d11
miwamiwa <6parsam@gmail.com>
2019-03-23 11:59:31 -0400

    Fixed random voice swapping

--- Assets/Sound Scripts/BGMchanges.cs
+++ Assets/Sound Scripts/BGMchanges.cs
@@ -8,16 +8,69 @@ public class BGMchanges : MonoBehaviour
     System.Random rnd;
     public int roomCounter = 0;
 
-    // Start is called before the first frame update
+    float counter = 0;
+    float bgmStarted = 0;
+    int loops;
+    int[] voicing = new int[1];
+
+    // 0 bass, 1 piano, 2 short, 3 short1, 4 med, 5 med1, 6 long, 7 long1
+
+    int[] lobbyCues = new int[5] { 0, 24, 48, 72, 96 };
+    int[][] lobbyVoicings = {
+
+        new int[] { 0, 2 },
+        new int[] { 0, 2, 7 },
+        new int[] { 0, 2, 1 },
+        new int[] { 0, 2, 7, 1 }
+    };
+
+    int[] room1Cues = new int[11] { 0, 6, 24, 36, 48, 60, 72, 96, 120, 132, 144 };
+    int[][] room1Voicings = {
+
+        new int[] { 0 },
+        new int[] { 0, 1  },
+        new int[] { 0, 1, 5, 7 },
+        new int[] { 0, 1  },
+        new int[] { 0, 1, 5, 7 },
+        new int[] { 0, 1, 5 },
+        new int[] { 0, 5, 7 },
+        new int[] { 0, 1, 5, 7 },
+        new int[] { 0, 1, 5 },
+        new int[] { 0, 5 }
+    };
+
+    int[] room2Cues = new int[7] { 0, 21, 41, 62, 83, 104, 126 };
+    int[][] room2Voicings = {
+
+        new int[] { 2 },
+        new int[] { 0, 1, 2 },
+        new int[] { 1, 2, 6 },
+        new int[] { 2, 6, 7  },
+        new int[] { 0, 1, 2, 7 },
+        new int[] { 0, 1, 2, 6, 7 }
+    };
+
+    int[] room3Cues = new int[8] { 0, 17, 51, 68, 85, 102, 119, 154 };
+    int[][] room3Voicings = {
+
+        new int[] { 0 },
+        new int[] { 0, 1, 3, 7 },
+        new int[] { 1, 6 },
+        new int[] { 1, 6, 2  },
+        new int[] { 1, 2, 3, 7 },
+        new int[] { 0, 1, 3 },
+        new int[] { 0, 3, 6 }
+    };
+
+    int[] lastStateChosen = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+
     void Start()
     {
-
     }
 
-    // Update is called once per frame
     void Update()
     {
-
     }
 
     public void SetRandomVoiceState()
@@ -26,124 +79,52 @@ public class BGMchanges : MonoBehaviour
         rnd = new System.Random();
 
         AkSoundEngine.GetState("BGM", out uint result);
-        //Debug.Log("this room's state ID is "+result);
+        // Debug.Log("this room's state ID is "+result);
 
         switch (result)
         {
-            case 1359360136: SetRandomRoom1Voice(); break;
-            case 1359360138: SetRandomRoom2Voice(); break;
-            case 1359360140: SetRandomRoom3Voice(); break;
-            case 3840192365: SetRandomStressTrackVoice(); break;
+            case 290285391: setRandomVoice(lobbyCues, lobbyVoicings); break;
+            case 1359360136: setRandomVoice(room1Cues, room1Voicings); break;
+            case 1359360138: setRandomVoice(room2Cues, room2Voicings); break;
+            case 1359360140: setRandomVoice(room3Cues, room3Voicings); break;
+                //  case 3840192365: setRandomVoice(); break;
 
         }
     }
 
-    void SetRandomStressTrackVoice()
+    void setRandomVoice(int[] cues, int[][] voicings)
     {
-        int voice = rnd.Next(1, 7);
-        int state3 = rnd.Next(1, 4);
-        int state4 = rnd.Next(1, 5);
 
-        switch (voice)
+        if (roomCounter == 0)
         {
-            case 1:
-                AkSoundEngine.SetState("guitar", "guit" + state4);
-                break;
-            case 2:
-                AkSoundEngine.SetState("keys", "state" + state3);
-                break;
-            case 3:
-                AkSoundEngine.SetState("mediumSounds", "state" + state4);
-                break;
-            case 4:
-                AkSoundEngine.SetState("shortSounds", "state" + state3);
-                break;
-            case 5:
-                AkSoundEngine.SetState("mediumSounds_01", "state" + state4);
-                break;
-            case 6:
-                AkSoundEngine.SetState("longSounds_01", "state" + state3);
-                break;
+            counter = Time.time;
         }
+        else
+        {
+            counter = Time.time - (3.2f);
+        }
+
+        // Debug.Log("time "+Time.time);
+        // Debug.Log("counter "+(counter-bgmStarted));
+
+        while (counter - bgmStarted > cues[cues.Length - 1])
+        {
+            counter -= cues[cues.Length - 1];
+        }
+
+        for (int i = 0; i < cues.Length - 1; i++)
+        {
+
+            if (counter - bgmStarted > cues[i] && counter - bgmStarted <= cues[i + 1])
+            {
+                voicing = voicings[i];
+            }
+        }
+
+        int randomPick = rnd.Next(0, voicing.Length);
+        SetRandomState(voicing[randomPick]);
     }
 
-    void SetRandomRoom3Voice()
-    {
-        int voice = rnd.Next(1, 7);
-        int state3 = rnd.Next(1, 4);
-        int state4 = rnd.Next(1, 5);
-
-        switch (voice)
-        {
-            case 1:
-                AkSoundEngine.SetState("guitar", "guit" + state4);
-                break;
-            case 2:
-                AkSoundEngine.SetState("keys", "state" + state3);
-                break;
-            case 3:
-                AkSoundEngine.SetState("longSounds", "state" + state3);
-                break;
-            case 4:
-                AkSoundEngine.SetState("shortSounds", "state" + state3);
-                break;
-            case 5:
-                AkSoundEngine.SetState("longSounds_01", "state" + state3);
-                break;
-            case 6:
-                AkSoundEngine.SetState("shortSounds_01", "state" + state3);
-                break;
-        }
-    }
-
-    void SetRandomRoom2Voice()
-    {
-        int voice = rnd.Next(1, 6);
-        int state3 = rnd.Next(1, 4);
-        int state4 = rnd.Next(1, 5);
-
-        switch (voice)
-        {
-            case 1:
-                AkSoundEngine.SetState("guitar", "guit" + state4);
-                break;
-            case 2:
-                AkSoundEngine.SetState("keys", "state" + state3);
-                break;
-            case 3:
-                AkSoundEngine.SetState("longSounds", "state" + state3);
-                break;
-            case 4:
-                AkSoundEngine.SetState("shortSounds", "state" + state3);
-                break;
-            case 5:
-                AkSoundEngine.SetState("longSounds_01", "state" + state3);
-                break;
-        }
-    }
-
-    void SetRandomRoom1Voice()
-    {
-        int voice = rnd.Next(1, 5);
-        int state3 = rnd.Next(1, 4);
-        int state4 = rnd.Next(1, 5);
-
-        switch (voice)
-        {
-            case 1:
-                AkSoundEngine.SetState("guitar", "guit" + state4);
-                break;
-            case 2:
-                AkSoundEngine.SetState("keys", "state" + state3);
-                break;
-            case 3:
-                AkSoundEngine.SetState("longSounds_01", "state" + state3);
-                break;
-            case 4:
-                AkSoundEngine.SetState("mediumSounds_01", "state" + state4);
-                break;
-        }
-    }
 
     public void SetAllStates(int state)
     {
@@ -156,73 +137,80 @@ public class BGMchanges : MonoBehaviour
         AkSoundEngine.SetState("mediumSounds_01", "state" + state);
         AkSoundEngine.SetState("shortSounds", "state" + state);
         AkSoundEngine.SetState("shortSounds_01", "state" + state);
+
+        for (int i = 0; i < lastStateChosen.Length; i++)
+        {
+            lastStateChosen[i] = state;
+        }
     }
 
     void SetRandomState(int voice)
     {
 
+        // Debug.Log("pick " + voice);
         int state3 = rnd.Next(1, 4);
         int state4 = rnd.Next(1, 5);
 
+        while (state3 == lastStateChosen[voice] || state4 == lastStateChosen[voice])
+        {
+            state3 = rnd.Next(1, 4);
+            state4 = rnd.Next(1, 5);
+        }
+
         switch (voice)
         {
-            case 1:
+            case 0:
                 AkSoundEngine.SetState("guitar", "guit" + state4);
+                lastStateChosen[0] = state4;
+                break;
+            case 1:
+                AkSoundEngine.SetState("keys", "state" + state3);
+                lastStateChosen[1] = state3;
                 break;
             case 2:
-                AkSoundEngine.SetState("keys", "state" + state3);
+                AkSoundEngine.SetState("shortSounds", "state" + state3);
+                lastStateChosen[2] = state3;
                 break;
             case 3:
-                AkSoundEngine.SetState("longSounds", "state" + state3);
+                AkSoundEngine.SetState("shortSounds_01", "state" + state3);
+                lastStateChosen[3] = state3;
                 break;
             case 4:
-                AkSoundEngine.SetState("longSounds_01", "state" + state3);
+                AkSoundEngine.SetState("mediumSounds", "state" + state4);
+                lastStateChosen[4] = state4;
                 break;
             case 5:
-                AkSoundEngine.SetState("mediumSounds", "state" + state4);
+                AkSoundEngine.SetState("mediumSounds_01", "state" + state4);
+                lastStateChosen[5] = state4;
                 break;
             case 6:
-                AkSoundEngine.SetState("mediumSounds_01", "state" + state4);
+                AkSoundEngine.SetState("longSounds", "state" + state3);
+                lastStateChosen[6] = state3;
                 break;
             case 7:
-                AkSoundEngine.SetState("shortSounds", "state" + state3);
-                break;
-            case 8:
-                AkSoundEngine.SetState("shortSounds_01", "state" + state3);
+                AkSoundEngine.SetState("longSounds_01", "state" + state3);
+                lastStateChosen[7] = state4;
                 break;
         }
     }
 
 
-public void SetAllToRandomState()
-{
-        rnd = new System.Random();
-        int state3 = rnd.Next(1, 4);
-        int state4 = rnd.Next(1, 5);
+    public void SetAllToRandomState()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            SetRandomState(i);
+        }
 
-    
-            AkSoundEngine.SetState("guitar", "guit" + state4);
-            AkSoundEngine.SetState("keys", "state" + state3);
-        state3 = rnd.Next(1, 4);
-        AkSoundEngine.SetState("longSounds", "state" + state3);
-        state3 = rnd.Next(1, 4);
-        AkSoundEngine.SetState("longSounds_01", "state" + state3);
-        state4 = rnd.Next(1, 5);
-        AkSoundEngine.SetState("mediumSounds", "state" + state4);
-        state4 = rnd.Next(1, 5);
-        AkSoundEngine.SetState("mediumSounds_01", "state" + state4);
-        state3 = rnd.Next(1, 4);
-        AkSoundEngine.SetState("shortSounds", "state" + state3);
-        state3 = rnd.Next(1, 4);
-        AkSoundEngine.SetState("shortSounds_01", "state" + state3);
-         
-}
+    }
 
     public void triggerRoomBGM()
     {
         // ------------- trigger next room bgm ---------------//
 
         AkSoundEngine.PostEvent("startBGM" + roomCounter, gameObject);
+
+        bgmStarted = Time.time;
         roomCounter += 1;
 
         // set instrument states
