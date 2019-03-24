2cb6132b2792079245aac6e653a3df031e612ad5
scott <scottjamessmith05@gmail.com>
2019-03-24 18:50:23 -0400

    hang level, move camera defualt set to perspective

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public float speed;
+    public float amount;
+    public float offset;
+
+    private void FixedUpdate()
+    {
+        this.transform.position += new Vector3(0, Mathf.Sin(Time.time * speed) + offset, 0) * amount;
+    }
+}
