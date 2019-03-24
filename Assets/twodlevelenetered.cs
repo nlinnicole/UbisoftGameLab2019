46d152075272658fa9d6bac5b40bae1b114905d7
Zied Jebali <ziedjebali@outlook.com>
2019-03-23 20:08:01 -0400

    2D Level Fixes
    gime those green squares

---
+++ Assets/twodlevelenetered.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class twodlevelenetered : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            anim.SetBool("StartLava", true);
+        }
+    }
+
+}
