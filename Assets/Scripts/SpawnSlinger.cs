350d223363757c3322f488378aece5441269bf58
scott <scottjamessmith05@gmail.com>
2019-03-25 14:04:00 -0400

    sling and zipline level started

---
+++ Assets/Scripts/SpawnSlinger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSlinger : MonoBehaviour
+{
+
+    public GameObject slingerPrefab;
+    public GameObject loadedSlinger;
+    public GameObject button;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 11)
+        {
+            loadedSlinger.GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.None;
+            loadedSlinger.GetComponentInChildren<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
+        }
+    }
+}
