3cdd06756b4b2b1f7cd0e838446f20b09ad68c14
Ricardo Liganor <ricardo.liganor@hotmail.com>
2019-03-26 23:29:09 -0400

    p2 added

--- Assets/Networking/GameManager.cs
+++ Assets/Networking/GameManager.cs
@@ -40,8 +40,8 @@ namespace Concordia1.Gamelab
             {
                 if (PhotonNetwork.IsMasterClient)
                     PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(22f, 5f, 10f), Quaternion.identity, 0);
-                //else
-                    //PhotonNetwork.Instantiate(this.playerPrefab2.name, new Vector3(-22f, 5f, 10f), Quaternion.identity, 0);
+                else
+                    PhotonNetwork.Instantiate(this.playerPrefab2.name, new Vector3(-22f, 5f, 10f), Quaternion.identity, 0);
             }
 
         }
