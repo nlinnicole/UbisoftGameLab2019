cbae1f6783cefe3e175374d824ec78096ef4744e
Zied Jebali <ziedjebali@outlook.com>
2019-03-26 21:03:39 -0400

    CAM WORKS FOR EACH TIME

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -5,18 +5,34 @@ using UnityEngine;
 public class MoveCamera : MonoBehaviour
 {
 
+    public GameObject DefaultCamT1, DefaultCamT2;
+
     private Transform start;
     public Transform Cam2D;
     public Transform CamDefault;
     public Transform CamTopDown;
     public Transform CamBehind;
 
+    public int teamnumber;
 
     public float cammovespeed = 1f;
+
+    public void Start()
+    {
+        start = gameObject.transform;
+        if (teamnumber == 1)
+        {
+            CamDefault = DefaultCamT2.transform;
+        }else if(teamnumber == -1)
+        {
+            CamDefault = DefaultCamT1.transform;
+        }
+    }
+
     public void changeTo2D()
     {
         start = gameObject.transform;
-        transform.position = Vector3.Slerp(start.position, Cam2D.position, Time.deltaTime * cammovespeed);
+        transform.position = Vector3.Lerp(start.position, Cam2D.position, Time.deltaTime * cammovespeed);
         transform.rotation = Quaternion.Slerp(start.rotation, Cam2D.rotation, Time.deltaTime * cammovespeed);
         gameObject.GetComponent<Camera>().orthographic = true;
 
@@ -25,8 +41,8 @@ public class MoveCamera : MonoBehaviour
     public void changetoDefault()
     {
         start = gameObject.transform;
-        transform.position = Vector3.Slerp(start.position, CamDefault.position, Time.deltaTime * cammovespeed);
-        transform.rotation = Quaternion.Slerp(start.rotation, CamDefault.rotation, Time.deltaTime * cammovespeed);
+        transform.position = Vector3.Slerp(start.position, new Vector3(CamDefault.position.x, CamDefault.position.y, CamDefault.position.z), Time.deltaTime * cammovespeed);
+        transform.rotation = Quaternion.Slerp(start.rotation, new Quaternion(CamDefault.rotation.x, CamDefault.rotation.y * teamnumber, CamDefault.rotation.z * teamnumber, CamDefault.rotation.w),Time.deltaTime * cammovespeed);
         gameObject.GetComponent<Camera>().orthographic = false;
 
     }
@@ -49,11 +65,7 @@ public class MoveCamera : MonoBehaviour
     }
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        start = gameObject.transform;
-    }
+    
 
     // Update is called once per frame
     void Update()
