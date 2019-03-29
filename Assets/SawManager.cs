db2ead814cf01e474ffed2d0d96eec2d957392f6
Zied Jebali <ziedjebali@outlook.com>
2019-03-29 00:24:08 -0400

    Lots of stuffs

---
+++ Assets/SawManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawManager : MonoBehaviour
+{
+    public float timer = 0f;
+
+    public float sawspeed = 2f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 6f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > 1.5f)
+        {
+            gameObject.transform.Translate(new Vector3(-1,0,0) * Time.deltaTime * sawspeed);
+        }
+        else
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, -1f) * Time.deltaTime);
+        }
+    }
+
+
+}
