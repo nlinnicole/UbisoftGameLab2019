b0ad86da36ddda54f87b346d686141716b9861fd
scott <scottjamessmith05@gmail.com>
2019-03-28 23:56:04 -0400

    saw fix

---
+++ Assets/Scripts/FinalDangle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalDangle : MonoBehaviour
+{
+
+    public float startFrame;
+
+    void Start()
+    {
+        GetComponent<Animator>().Play("GOGOGO", 0, startFrame);
+    }
+}
