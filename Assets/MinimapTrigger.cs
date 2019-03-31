b1f2d2b3412bd0165f2b718d51cdefa399c953d9
Zied Jebali <ziedjebali@outlook.com>
2019-03-31 14:09:07 -0400

    Pog

---
+++ Assets/MinimapTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapTrigger : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Player = GameObject.FindGameObjectWithTag("Minimap");
+            Player.GetComponent<WhichRoomAreYouIn>().Team1Position = other.gameObject;
+            Player.GetComponent<WhichRoomAreYouIn>().StartFindingDistance = true;
+            Player.GetComponent<WhichRoomAreYouIn>().enabled = true;
+        }
+    }
+}
